371c5a2adfb493defdceedc94744a3baaaa745c0
Francis Poliquin <francispoliquin@gmail.com>
2022-11-30 11:50:57 -0500

    improved routing, added new people component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StarshipListComponent } from './starship-list/starship-list.component';
 import { StarshipDetailsComponent } from './starship-details/starship-details.component';
+import { StarshipListItemComponent } from './starship-list-item/starship-list-item.component';
 import { ErrorComponent } from './error/error.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 import { PeopleDetailsComponent } from './people-details/people-details.component';
 import { DataService } from './data.service';
 import { LoadingComponent } from './loading/loading.component';
@@ -19,6 +21,7 @@ import { LoadingComponent } from './loading/loading.component';
     NavBarComponent,
     StarshipListComponent,
     StarshipDetailsComponent,
+    StarshipListItemComponent,
     ErrorComponent,
     PeopleDetailsComponent,
     LoadingComponent,
@@ -27,9 +30,11 @@ import { LoadingComponent } from './loading/loading.component';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: '', component: StarshipListComponent },
       { path: 'starships/:starshipName', component: StarshipDetailsComponent },
+      { path: 'starships', component: StarshipListComponent },
+      { path: 'people', component: PeopleListComponent },
       { path: 'people/:personName', component: PeopleDetailsComponent },
+      { path: '', redirectTo: 'starships', pathMatch: 'full' }, // redirects to landing page
       { path: '**', component: ErrorComponent },
     ]),
     AppRoutingModule,
